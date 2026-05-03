5dd126a8b22adf9a892e6988a506c67abb11df6d
suman haldar <sssseeeettttjjjj@gmail.com>
2026-05-03 18:41:51 +0530

    updated compose, server.js

--- app/server.js
+++ app/server.js
@@ -5,4 +5,9 @@ app.use(express.static("public"));
 
 app.get("/health", (req, res) => res.send("OK"));
 
+app.get("/environment", (req, res) => {
+  const environment = process.env.ENVIRONMENT || "unknown";
+  res.json({ environment });
+});
+
 app.listen(3000, () => console.log("App running on port 3000"));
